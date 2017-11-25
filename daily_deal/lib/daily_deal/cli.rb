0209eaabf837836ea55be97fdd0942d541c6ad38
Aaron Samuel <aasamuelj@gmail.com>
2017-11-25 20:16:22 +0000

    Done.

--- daily_deal/lib/daily_deal/cli.rb
+++ daily_deal/lib/daily_deal/cli.rb
@@ -10,25 +10,30 @@ class DailyDeal::CLI
     # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
     puts "Todays Daily Deals:"
     @deals = DailyDeal::Deal.today
+    @deals.each.with_index(1) do |deal, i|
+      puts "#{i}. #{deal.name} - #{deal.price} - #{deal.availability}"
+    end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the deal you'd like more info on or type list to see the deals again or type exit"
+      puts "Enter the number of the deal you'd like more info on or type list to see the deals again or type exit:"
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "More info on deal 1..."
-      when "2"
-        puts "More info on deal 2..."
-      when "list"
+
+      if input.to_i > 0
+        the_deal = @deals[input.to_i-1]
+        puts "#{the_deal.name} - #{the_deal.price} - #{the_deal.availability}"
+      elsif input == "list"
         list_deals
+      elsif input == "exit"
+        puts "Thank you!"
       else
         puts "Not sure what you want, type list or exit."
       end
     end
   end
+
   def goodbye
     puts "See you tomorrow for more deals!!!"
   end
--- daily_deal/lib/daily_deal/deal.rb
+++ daily_deal/lib/daily_deal/deal.rb
@@ -1,23 +1,56 @@
 class DailyDeal::Deal
-  attr_accessor :name, :price, :availablility, :url
+  attr_accessor :name, :price, :availability, :url
   def self.today
-    # I should return a bunch of instances of deals
-    puts <<-DOC.gsub /^\s*/, ''
-      1. PCH Digital Pulse Massager - $27 - Still Available!
-      2. Lenovo ThinkPad 11E 11.6 - $199.99 - Still Available!
-    DOC
+    # Scrape Woot and Meh and then return deals based on that data
+    self.scrape_deals
+  end
 
-    deal_1 = self.new
-    deal_1.name = "PCH Digital Pulse Massager"
-    deal_1.price = "$27"
-    deal_1.availablility = true
-    deal_1.url = "https://meh.com/"
+  def self.scrape_deals
+    deals = []
 
-    deal_2 = self.new
-    deal_2.name = "Lenovo ThinkPad 11E 11.6"
-    deal_2.price = "$199.99"
-    deal_2.availablility = true
-    deal_2.url = "http://www.woot.com/offers/lenovo-thinkpad-11-6-quad-core"
-    [deal_1, deal_2]
+    deals << self.scrape_woot
+    deals << self.scrape_meh
+    # Go to woot, find the product
+    # extract the properties
+    # instantiate a deal
+
+    # Go to meh...
+
+    # deal_1 = self.new
+    # deal_1.name = "PCH Digital Pulse Massager"
+    # deal_1.price = "$27"
+    # deal_1.availablility = true
+    # deal_1.url = "https://meh.com/"
+    #
+    # deal_2 = self.new
+    # deal_2.name = "Lenovo ThinkPad 11E 11.6"
+    # deal_2.price = "$199.99"
+    # deal_2.availablility = true
+    # deal_2.url = "http://www.woot.com/offers/lenovo-thinkpad-11-6-quad-core"
+    # [deal_1, deal_2]
+    deals
+  end
+  def self.scrape_woot
+    doc = Nokogiri::HTML(open("https://woot.com"))
+    # DailyDeal::Deal.scrape_woot
+    #doc.to_html
+    deal = self.new
+    deal.name = doc.search("h2.main-title").text
+    deal.price = doc.search("div.price-holder span.price").text
+    deal.availability = true
+    deal.url = doc.search("a.wantone").first.attr("href")
+    deal
+  end
+
+  def self.scrape_meh
+    doc = Nokogiri::HTML(open("https://meh.com/"))
+    # DailyDeal::Deal.scrape_meh
+    #doc.to_html
+    deal = self.new
+    deal.name = doc.search("section.features h2").text.strip
+    deal.price = doc.search("button.buy-button").text.gsub("Buy it","").strip
+    deal.availability = true
+    deal.url = "https://meh.com"
+    deal
   end
 end
