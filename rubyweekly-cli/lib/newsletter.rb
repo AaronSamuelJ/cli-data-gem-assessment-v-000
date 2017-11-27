97b15d6b6cead5b547dd63055920910032e60a3d
Aaron Samuel <aasamuelj@gmail.com>
2017-11-27 19:09:31 +0000

    Done.

--- rubyweekly-cli/lib/newsletter.rb
+++ rubyweekly-cli/lib/newsletter.rb
@@ -1,4 +1,22 @@
 class Newsletter
-  attr_accessor :articles, :issue_number, :issue_date
+  attr_accessor :issue_number, :issue_date
+  attr_reader :articles
 
-end 
+  def initialize
+    @articles = []
+  end
+
+  def articles
+    @articles.dup.freeze
+  end
+
+  def add_article(article)
+    raise "invalid article" if !article.is_a?(Article)
+  end
+
+  def first_article_url
+    # articles is an array
+    # the first element in that array must respond to a URL method
+    self.articles.first.url
+  end
+end
